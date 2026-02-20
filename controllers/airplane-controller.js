5bd64a761ec44931cd36c800dda8b46898aef695
akil <akileshjm@gmail.com>
2026-02-21 01:48:56 +0530

    Fixed code

---
+++ controllers/airplane-controller.js
@@ -0,0 +1,73 @@
+const {StatusCodes} = require('http-status-codes');
+
+const {AirplaneService} = require('../services');
+const {SuccessResponse,ErrorResponse} = require('../utils/common');
+/**
+ * POST:/airplanes
+ *  req -body {modelumber: 'airbus320' ,capacity:200}
+ * res 
+ */
+async function createAirplane(req,res) {
+    try {
+       
+        const airplane = await AirplaneService.createAirplane({
+                modelNumber: req.body.modelNumber,
+                capacity: req.body.capacity
+        });
+        SuccessResponse.data = airplane;
+        return res
+        .status(StatusCodes.CREATED)
+        .json(SuccessResponse); 
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res
+        .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+        .json(ErrorResponse);
+    }
+};
+
+   /**
+ * GET:/airplanes/:id
+ *  req -body {}
+ * res 
+ */
+    async function getAirplanes(req,res) {
+        try {
+            const airplane = await AirplaneService.getAirplanes();
+            SuccessResponse.data = airplane;
+        return res
+        .status(StatusCodes.OK)
+        .json(SuccessResponse); 
+        } catch (error) {
+            ErrorResponse.error = error;
+        return res
+        .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+        .json(ErrorResponse);
+        }
+    };
+  /**
+ * GET:/airplanes/:id
+ *  req -body {}
+ * res 
+ */
+    async function getAirplane(req,res) {
+        try {
+            const airplane = await AirplaneService.getAirplane(req.params.id);
+            SuccessResponse.data = airplane;
+        return res
+        .status(StatusCodes.OK)
+        .json(SuccessResponse); 
+        } catch (error) {
+            ErrorResponse.error = error;
+        return res
+        .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+        .json(ErrorResponse);
+        }
+    };
+
+
+module.exports = {
+    createAirplane,
+    getAirplanes,
+    getAirplane
+};
